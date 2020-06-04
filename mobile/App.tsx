cd5b50f7beef094bd6d9d55de69d9e72fab86d06
Johnatan Dantas <dantas.johnatan@gmail.com>
2020-06-04 17:11:26 -0300

    feat: add mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'react-native';
+
+import { Ubuntu_700Bold } from '@expo-google-fonts/ubuntu';
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+
+import Routes from './src/routes';
+
+export default function App() {
+  const [fontsLoader] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold,
+  });
+
+  if (!fontsLoader) {
+    return <AppLoading />;
+  }
+  return (
+    <>
+      <StatusBar
+        barStyle='dark-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Routes />
+    </>
+  );
+}
